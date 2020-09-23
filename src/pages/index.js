d81040859aab9e410338ea89e43229359674f1aa
Kevin Shreve <kshreve@wayup.com>
2020-09-23 14:26:42 -0400

    test gatsby deploy2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world! Testing deploying to netlify</div>
+  return <div>Hello world! Testing deploying to ???</div>
 }
